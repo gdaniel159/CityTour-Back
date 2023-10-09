6185cfc39dbafa6990e70b6713590c06b49c9198
aldo <samuelchuno2000@gmail.com>
2023-10-08 20:22:15 -0500

    Se termino el modulo de tipo_usuario, ya funcuiona de forma correcta

--- app/Http/Controllers/TipoUsuarioController.php
+++ app/Http/Controllers/TipoUsuarioController.php
@@ -46,13 +46,59 @@ class TipoUsuarioController extends Controller
 
     // PUT - Actualizar datos
 
-    public function update(Request $request, $id)
+   public function update(Request $request, $id)
     {
+        DB::beginTransaction();
+        try {
+            // Obtenemos el tipo de usuario por su ID
+            $tipo_usuario = TipoUsuario::find($id);
+            
+            // Verificamos si el tipo de usuario existe
+            if (!$tipo_usuario) {
+                return response()->json(['error' => 'Tipo de usuario no encontrado'], 404);
+            }
+            
+            // Actualizamos el nombre si se proporciona en la solicitud
+            if ($request->has('nombre')) {
+                $tipo_usuario->nombre = $request->nombre;
+            }
+
+            // Guardamos los cambios
+            $tipo_usuario->save();
+
+            DB::commit();
+
+            // Retornamos un mensaje
+            return response()->json(['message' => 'Tipo de usuario actualizado correctamente'], 200);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(["error" => "Error al actualizar el tipo de usuario: " . $e->getMessage()], 500);
+        }
     }
 
     // DELETE - Eliminar datos
-
     public function delete(Request $request, $id)
     {
+        DB::beginTransaction();
+        try {
+            // Obtenemos el tipo de usuario por su ID
+            $tipo_usuario = TipoUsuario::find($id);
+            
+            // Verificamos si el tipo de usuario existe
+            if (!$tipo_usuario) {
+                return response()->json(['error' => 'Tipo de usuario no encontrado'], 404);
+            }
+
+            // Eliminamos el tipo de usuario
+            $tipo_usuario->delete();
+
+            DB::commit();
+
+            // Retornamos un mensaje
+            return response()->json(['message' => 'Tipo de usuario eliminado correctamente'], 200);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(["error" => "Error al eliminar el tipo de usuario: " . $e->getMessage()], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Route;
 // TIPO DE USUARIO
 Route::get('tipo_usuario/get',[TipoUsuarioController::class,'get']);
 Route::post('tipo_usuario/create',[TipoUsuarioController::class,'store']);
+Route::put('tipo_usuario/update/{id}', [TipoUsuarioController::class, 'update']); // Ruta para actualizar
+Route::delete('tipo_usuario/delete/{id}', [TipoUsuarioController::class, 'delete']); // Ruta para eliminar
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
